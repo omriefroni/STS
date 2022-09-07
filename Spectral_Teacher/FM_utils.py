c81b6822aeea91fb876b820613ae27cd9c62abc8
omriefroni <efroni.omri7@gmail.com>
2022-09-07 14:48:09 +0300

    add spectral utils: fm_step and fm_loss

---
+++ Spectral_Teacher/FM_utils.py
@@ -0,0 +1,80 @@
+import torch
+from Spectral_Teacher.surfmnet_loss import SURFMNetLoss
+
+
+
+
+def fm_step(batch, feat1, feat2):
+    """
+    Functional Map step - calculate 2 mapping matrices C at the spectral space, and SURFMNet loss.  
+
+    Inpute: 
+    - batch dictionary contains: 
+        B- batch, N - number of points, K - number of LBO vectors, last dim: 0 - source, 1 - target
+        LBO data:
+        * 'evects' - LBO eigenvectors - [B, N, K, 2] 
+        * 'evals' - LBO eigenvalues - [B, K, 2]
+        * 'a' - faces areas for normalization [B, N, 2] - OR: 'evects_trans' - normerlized LBO eigenvectors. 
+
+        Features (from the student): 
+        D - features dimensions 
+        * feat1 - per-point features of the source - [N, D]
+        * feat2 - per-point features of the target - [N, D]
+
+
+    """
+    # Loss definition
+    fm_loss = SURFMNetLoss()
+
+
+    # extract variables from batch
+    evects1 = batch['evects'][:, :, :, 0]
+    evects2 = batch['evects'][:, :, :, 1]
+    A1 = batch['a'][:, :, 0]
+    A2 = batch['a'][:, :, 1]
+    evals_1 = batch['evals'][:,:,0]
+    evals_2 = batch['evals'][:,:,1]
+    if hasattr(batch, 'evects_trans'):
+        evects1_trans = batch['evects_trans'][:, :, :, 0]
+        evects2_trans = batch['evects_trans'][:, :, :, 0]
+    else:
+        evects1_trans = evects1.permute((0,2,1)) * A1[:,None,:]
+        evects2_trans = evects2.permute((0,2,1)) * A2[:,None,:]
+
+    f_hat = torch.bmm(evects1_trans, feat1)
+    g_hat = torch.bmm(evects2_trans, feat2)
+    f_hat, g_hat = f_hat.transpose(1, 2), g_hat.transpose(1, 2)
+
+    C1 = []
+    C2 = []
+    # P1 = []
+    # P2 = []
+
+    for i in range(feat1.shape[0]):
+        fm1 = torch.matmul(feat1.transpose(2, 1)[i], evects1_trans.transpose(2, 1)[i])
+        fm2 = torch.matmul(feat2.transpose(2, 1)[i], evects2_trans.transpose(2, 1)[i])
+
+
+        fm1_inv = fm1.transpose(1,0).pinverse()
+
+        c_mat = torch.matmul(fm2.transpose(1,0), fm1_inv)
+        C1.append(c_mat.unsqueeze(0))
+
+        # if is_plot:
+        #     p1 = FM_to_p2p(c_mat, evects1[i], evects2[i])
+        #     P1.append(p1)
+
+        fm2_inv = fm2.transpose(1,0).pinverse()
+        c_mat = torch.matmul(fm1.transpose(1,0), fm2_inv)
+        C2.append(c_mat.unsqueeze(0))
+
+        # if is_plot:
+        #     p2 = FM_to_p2p(c_mat, evects2[i], evects1[i])
+        #     P2.append(p2)
+
+    C1 = torch.cat(C1, dim=0)
+    C2 = torch.cat(C2, dim=0)
+
+    curr_loss = fm_loss(C1, C2, feat1, feat2, evects1, evects2,  evects1_trans, evects2_trans, evals_1, evals_2, C1.device )
+    return curr_loss
+
---
+++ Spectral_Teacher/surfmnet_loss.py
@@ -0,0 +1,94 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+# Borrowed from: https://github.com/pvnieo/SURFMNet-pytorch/blob/master/surfmnet/loss.py
+
+class SURFMNetLoss(nn.Module):
+    """
+    Calculate the loss as presented in the SURFMNet paper.
+    """
+    def __init__(self, w_bij=1e3, w_orth=1e3, w_lap=1, w_pre=1e5, sub_pre=0.2):
+        """Init SURFMNetLoss
+        Keyword Arguments:
+            w_bij {float} -- Bijectivity penalty weight (default: {1e3})
+            w_orth {float} -- Orthogonality penalty weight (default: {1e3})
+            w_lap {float} -- Laplacian commutativity penalty weight (default: {1})
+            w_pre {float} -- Descriptor preservation via commutativity penalty weight (default: {1e5})
+            sub_pre {float} -- Percentage of subsampled vertices used to compute
+                               descriptor preservation via commutativity penalty (default: {0.2})
+        """
+        super().__init__()
+        self.w_bij = w_bij
+        self.w_orth = w_orth
+        self.w_lap = w_lap
+        self.w_pre = w_pre
+        self.sub_pre = sub_pre
+
+    def forward(self, C1, C2, feat_1, feat_2, evecs_1, evecs_2, evecs_trans_1, evecs_trans_2, evals_1, evals_2, device):
+        """Compute soft error loss
+        Arguments:
+            C1 {torch.Tensor} -- matrix representation of functional correspondence.
+                                Shape: batch_size x num-eigenvectors x num-eigenvectors.
+            C2 {torch.Tensor} -- matrix representation of functional correspondence.
+                                Shape: batch_size x num-eigenvectors x num-eigenvectors.
+            feat_1 {Torch.Tensor} -- learned feature 1. Shape: batch-size x num-vertices x num-features
+            feat_2 {Torch.Tensor} -- learned feature 2. Shape: batch-size x num-vertices x num-features
+            evecs_1 {Torch.Tensor} -- eigen vectors decomposition of shape 1. Shape: batch-size x num-vertices x num-eigenvectors
+            evecs_2 {Torch.Tensor} -- eigen vectors decomposition of shape 2. Shape: batch-size x num-vertices x num-eigenvectors
+            evecs_trans_1: {Torch.Tensor} -- inverse eigen vectors decomposition of shape 1. defined as evecs_x.t() @ mass_matrix.
+                                             Shape: batch-size x num-eigenvectors x num-vertices
+            evecs_trans_2: {Torch.Tensor} -- inverse eigen vectors decomposition of shape 2. defined as evecs_y.t() @ mass_matrix.
+                                             Shape: batch-size x num-eigenvectors x num-vertices
+            evals_1 {Torch.Tensor} -- eigen values of shape 1. Shape: batch-size x num-eigenvectors
+            evals_2 {Torch.Tensor} -- eigen values of shape 2. Shape: batch-size x num-eigenvectors
+            device {Torch.device} -- device used (cpu or gpu)
+        Returns:
+            float -- total loss
+        """
+        criterion = FrobeniusLoss()
+        eye = torch.eye(C1.size(1), C1.size(2)).unsqueeze(0)
+        eye_batch = torch.repeat_interleave(eye, repeats=C1.size(0), dim=0).to(device)
+
+        # Bijectivity penalty
+        bijectivity_penalty = criterion(torch.bmm(C1, C2), eye_batch) + criterion(torch.bmm(C2, C1), eye_batch)
+        bijectivity_penalty *= self.w_bij
+
+        # Orthogonality penalty
+        orthogonality_penalty = criterion(torch.bmm(C1.transpose(1, 2), C1), eye_batch)
+        orthogonality_penalty += criterion(torch.bmm(C2.transpose(1, 2), C2), eye_batch)
+        orthogonality_penalty *= self.w_orth
+
+        # Laplacian commutativity penalty
+        laplacian_penalty = criterion(torch.einsum('abc,ac->abc', C1, evals_1), torch.einsum('ab,abc->abc', evals_2, C1))
+        laplacian_penalty += criterion(torch.einsum('abc,ac->abc', C2, evals_2), torch.einsum('ab,abc->abc', evals_1, C2))
+        laplacian_penalty *= self.w_lap
+
+        # Descriptor preservation via commutativity
+        # see `Informative Descriptor Preservation via Commutativity for Shape Matching` for more information
+        # http://www.lix.polytechnique.fr/~maks/papers/fundescEG17.pdf
+        num_desc = int(feat_1.size(2) * self.sub_pre)
+        descs = np.random.choice(feat_1.size(2), num_desc)
+        feat_1 = feat_1[:, :, descs].transpose(1, 2).unsqueeze(2)
+        feat_2 = feat_2[:, :, descs].transpose(1, 2).unsqueeze(2)
+        M_1 = torch.einsum('abcd,ade->abcde', feat_1, evecs_1)
+        M_1 = torch.einsum('afd,abcde->abcfe', evecs_trans_1, M_1)
+        M_2 = torch.einsum('abcd,ade->abcde', feat_2, evecs_2)
+        M_2 = torch.einsum('afd,abcde->abcfe', evecs_trans_2, M_2)
+        C1_expand = torch.repeat_interleave(C1.unsqueeze(1).unsqueeze(1), repeats=num_desc, dim=1)
+        C2_expand = torch.repeat_interleave(C2.unsqueeze(1).unsqueeze(1), repeats=num_desc, dim=1)
+        source1, target1 = torch.einsum('abcde,abcef->abcdf', C1_expand, M_1), torch.einsum('abcef,abcfd->abced', M_2, C1_expand)
+        source2, target2 = torch.einsum('abcde,abcef->abcdf', C2_expand, M_2), torch.einsum('abcef,abcfd->abced', M_1, C2_expand)
+        preservation_penalty = criterion(source1, target1) + criterion(source2, target2)
+        preservation_penalty *= self.w_pre
+
+        return bijectivity_penalty + orthogonality_penalty + laplacian_penalty + preservation_penalty
+
+
+class FrobeniusLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, a, b):
+        loss = torch.sum((a - b) ** 2, axis=(1, 2))
+        return torch.mean(loss)
